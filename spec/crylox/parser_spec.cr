2d1d4dd97e2abadc2113ce85bef2f82e138de8d1
Margret Riegert <margret@eowyn.net>
2023-09-22 02:42:50 -0400

    rename parse_source in spec

--- spec/crylox/parser_spec.cr
+++ spec/crylox/parser_spec.cr
@@ -1,6 +1,6 @@
 require "../spec_helper"
 
-def parse_source(source : String) : String?
+def parse_source_to_print_ast(source : String) : String?
   scanner = Crylox::Scanner.new(source)
   tokens = scanner.scan_tokens
 
@@ -16,14 +16,14 @@ end
 
 describe Crylox::Parser do
   it "parses integers from ast" do
-    parse_source("1 + 1").should eq("(+ 1.0 1.0)")
+    parse_source_to_print_ast("1 + 1").should eq("(+ 1.0 1.0)")
   end
 
   it "parses strings from ast" do
-    parse_source("\"my string\"").should eq("\"my string\"")
+    parse_source_to_print_ast("\"my string\"").should eq("\"my string\"")
   end
 
   it "removes comments" do
-    parse_source("(1 + 1) == 2 // need to double check").should eq("(== (group (+ 1.0 1.0)) 2.0)")
+    parse_source_to_print_ast("(1 + 1) == 2 // need to double check").should eq("(== (group (+ 1.0 1.0)) 2.0)")
   end
 end
